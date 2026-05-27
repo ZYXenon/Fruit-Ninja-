eccb3f0d8aaa7b211353cecd73a9f516b33eeaa3
L-Ashcroft <3179386694@qq.com>
2026-05-27 21:20:18 +0800

    Ignore generated build artifacts

--- software/ninja/obj/default/driver_ov2640.d
+++
@@ -1,3 +0,0 @@
-obj/default/driver_ov2640.o: driver_ov2640.c driver_ov2640.h
-
-driver_ov2640.h:
--- software/ninja/obj/default/driver_ov2640_basic.d
+++
@@ -1,8 +0,0 @@
-obj/default/driver_ov2640_basic.o: driver_ov2640_basic.c \
- driver_ov2640_basic.h driver_ov2640_interface.h driver_ov2640.h
-
-driver_ov2640_basic.h:
-
-driver_ov2640_interface.h:
-
-driver_ov2640.h:
